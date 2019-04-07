d721f3d8ed4e6229549905d0aaf3d4260ba614df
Leos Julien <leos.julien@gmail.com>
2019-04-07 18:28:22 +0200

    add project

---
+++ pages/foo.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Foo
+      </h1>
+      <h2 class="subtitle">
+        A Foo page
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  // layout: 'different'
+}
+</script>
+
+<style>
+</style>
